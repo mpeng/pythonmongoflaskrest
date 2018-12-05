ea05d61fa71dc9d5b19e1bb609150bfc5ad002bd
mpeng <mpeng@bridge2solutions.com>
2018-12-05 15:31:08 -0500

    adding shutdown

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ from pymongo import MongoClient
 from bson.json_util import dumps
 import json
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
 client = MongoClient('localhost:27017')
 db = client.ContactDB
 
